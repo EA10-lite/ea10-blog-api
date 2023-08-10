0b8056fe1ef868649bb03dc9d3bf02911aaae877
Emmanuel <emanuelanyigor@gmail.com>
2023-08-10 12:22:15 -0700

    updating cors

--- startup/cors.js
+++ startup/cors.js
@@ -1,8 +1,8 @@
 const cors = require("cors");
 
 const options = {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    origin: ["http://localhost: 5173", "http://localhost:3000", "http://localhost:3001"],
+    'Access-Control-Allow-Origin': 'http://localhost:5173',
     'Access-Control-Allow-Credentials': true,
     credentials: true 
 }

5419aec9a4a398502db4f5f169b29a942f8e3b70
Emmanuel <emanuelanyigor@gmail.com>
2023-08-10 15:31:07 -0700

    updating db startup

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const logger = require("../utils/logger");
 
 module.exports = () => {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true })
         .then(()=> logger.log("connected to mongodb"))
         .catch(err => logger.error("failed to connect to mongodb", err))
 }
